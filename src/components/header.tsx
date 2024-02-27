1cfb816ee92983c83b13448c4b488f96fd732ffd
hnin <hnin0929@ecplaza.net>
2024-02-27 17:33:23 +0900

    header

---
+++ src/components/header.tsx
@@ -0,0 +1,11 @@
+import Link from 'next/link';
+
+export default function Header() {
+    return (
+        <div>
+            <Link href="/performance">Performance</Link>
+            <Link href="/reliability">reliability</Link>
+            <Link href="/scale">scale</Link>
+        </div>
+    );
+}
